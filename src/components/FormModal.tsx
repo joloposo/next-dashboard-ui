7e6f809d070c887f2f9f5e56eb3b08428f261d7e
Michal Vavrek <vavrek@duxsoft.sk>
2024-10-10 18:24:20 +0200

    Form modal design

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 function FormModal({
   table,
@@ -29,11 +29,37 @@ function FormModal({
   const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
   const bgColor = type === "create" ? "bg-lamaYellow" : type === "update" ? "bg-lamaSky" : "bg-lamaPurple";
 
+  const [open, setOpen] = useState(false);
+
+  const Form = () => {
+    return type === "delete" && id ? (
+      <form action="" className="flex flex-col p-4  gap-4">
+        <span className="text-center font-medium">All data will be lost. Are tou sure you want to delete {table}?</span>
+        <button type="submit" className="bg-red-700 text-white rounded-md py-2 px-4 border-none w-max self-center">Delete</button>
+      </form>
+    ) : (
+      "create or update"
+    );
+  };
+
   return (
     <>
-      <button className={`${size} flex items-center justify-center rounded-full ${bgColor}`}>
-        <Image src={`/${type}.png`} alt='' width={16} height={16} /> 
+      <button
+        className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
+        onClick={() => setOpen(true)}
+      >
+        <Image src={`/${type}.png`} alt="" width={16} height={16} />
       </button>
+      {open && (
+        <div className="w-full h-full absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+          <div className="bg-white rounded-md p-4 relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
+            <Form />
+            <div className="absolute top-4 right-4 cursor-pointer" onClick={() => setOpen(false)}>
+              <Image src={"/close.png"} alt="close" width={14} height={14} />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
